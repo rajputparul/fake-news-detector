b664b66ff51e9c40353902be2e7022baf14d746b
rajputparul <parulthakur313@gmail.com>
2026-04-04 18:56:51 +0530

    Update success message format

--- model_training.py
+++ model_training.py
@@ -4,26 +4,20 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.linear_model import LogisticRegression
 import pickle
 
-# Load dataset
 df = pd.read_csv("FakeNewsNet.csv")
 
-# # Data
 X = df['title'] 
 y = df['real']    
 
-# Split
 x_train, x_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
 
-# Vectorization
 vectorizer = TfidfVectorizer(stop_words='english')
 x_train_vec = vectorizer.fit_transform(x_train)
 
-# Model
 model = LogisticRegression()
 model.fit(x_train_vec, y_train)
 
-# Save
 pickle.dump(model, open("model.pkl", "wb"))
 pickle.dump(vectorizer, open("vectorizer.pkl", "wb"))
 
-print("✅ Model trained & saved!")
+print(" Model trained & saved!")
